b2bcea293666fa04dc12ab0a7cb371a2b1f67dc3
Bruce0203 <BruceFreedy@gmail.com>
2023-08-23 20:39:17 +0900

    fix: ignore mouse movement

--- src/main/kotlin/io/github/bruce0203/glowonmove/MoveListener.kt
+++ src/main/kotlin/io/github/bruce0203/glowonmove/MoveListener.kt
@@ -2,17 +2,17 @@ package io.github.bruce0203.glowonmove
 
 import io.github.bruce0203.glowonmove.event.PlayerGlowEvent
 import org.bukkit.Bukkit
+import org.bukkit.Location
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerMoveEvent
-import org.bukkit.util.Vector
 
 class MoveListener(private val config: Config = DEFAULT_CONFIG) : Listener {
 
     @Suppress("UNUSED")
     @EventHandler
     fun onMove(event: PlayerMoveEvent) {
-        if (event.from.clone().setDirection(Vector()) == event.to.clone().setDirection(Vector())) return
+        if (event.to.isSamePosition(event.from)) return
         val player = event.player
         if (player.isOp) return
         val extraPlayer = PlayerExtra[player]
@@ -28,6 +28,7 @@ class MoveListener(private val config: Config = DEFAULT_CONFIG) : Listener {
         }
     }
 
+    fun Location.isSamePosition(t: Location) = world == t.world && x == t.x && y == t.y && z == t.z
 
 
 }
